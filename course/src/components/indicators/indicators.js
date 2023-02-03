f5fd660712b02dec634bf8554bbe0555ecce5118
cpribaz <cpribaz@uwo.ca>
2023-02-02 19:08:40 -0500

    [ST-20] CP: fixed overlap with footer

--- course/src/components/indicators/indicators.js
+++ course/src/components/indicators/indicators.js
@@ -34,49 +34,76 @@ function Indicators(){
                 <button id="11" class="catagoryBtn" onClick={showIndicator}>11 - Economics and Project Management</button>
                 <button id="12" class="catagoryBtn" onClick={showIndicator}>12 - Life-Long Learning</button>
             </div>
-            <div class="description" id="description">
+            <div class="tables">
                 <div id="table1">
+                    <h2>1 - Knowledge Base</h2>
+                    <p>Demonstrated competence in university level mathematics, natural sciences, engineering fundamentals, and specialized engineering knowledge appropriate to the program. (Base primarily on performance on Final Examinations for first year courses.  Note this attribute is knowledge, not application, so “State Newton’s Third Law of Motion” is appropriate for this attribute whereas “Apply Newton’s Third Law of Motion to truss analysis using the Method of Sections” (or “Conduct Truss Analysis using the Method of Sections”) is Problem Analysis, Attribute 2).</p>
                     <Table1/>
                 </div>
                 <div id="table2">
+                    <h2>2 - Problem Analysis</h2>
+                    <p>An ability to use appropriate knowledge and skills to identify, formulate, analyze and solve complex engineering problems in order to reach substantiated conclusions. (Applies to “one correct answer” problems and open-ended design problems)</p>
                     <Table2/>
                 </div>
                 <div id="table3">
+                    <h2>3 - Investigation</h2>
+                    <p>An ability to conduct investigations of complex problems by methods that include appropriate experiments, analysis and interpretation of data, and synthesis of information in order to reach valid conclusions.</p>
                     <Table3/>
                 </div>
                 <div id="table4">
+                    <h2>4 - Design</h2>
+                    <p>An ability to design solutions for complex, open-ended engineering problems and to design systems, components or processes that meet specified needs with appropriate attention to health and safety risks, applicable standards, and economic, environmental, cultural and societal considerations.</p>
                     <Table4/>
                 </div>
                 <div id="table5">
+                    <h2>5 - Use of Engineering Tools</h2>
+                    <p>An ability to create, select, apply, adapt, and extend appropriate techniques, resources, and modern engineering tools to a range of engineering activities, from simple to complex, with an understanding of the associated limitations.</p>
+                    <ul>The scope of “Engineering Tools” (ET) includes
+                        <li>Basic and advanced numerical modelling tools (spreadsheets, Matlab)</li>
+                        <li>Computer-aided Engineering (CAx) Tools (design software, finite element analysis, 3-D printing)</li>
+                        <li>Conventional engineering databases and resources (data/formula tables in handbooks/catalogues, materials specifications)</li>
+                    </ul>
                     <Table5/>
                 </div>
                 <div id="table6">
+                    <h2>6 - Individual and Team Work</h2>
+                    <p>An ability to work effectively as a member and leader in teams preferably in a multidisciplinary setting</p>
                     <Table6/>
                 </div>
                 <div id="table7">
+                    <h2>7 - Communication Skills</h2> 
+                    <p>An ability to communicate complex engineering concepts within the profession and with society at large. Such ability includes reading, writing, speaking and listening, and the ability to comprehend and write effective reports and design documentation, and to give and effectively respond to clear instructions</p>
                     <Table7/>
                 </div>
                 <div id="table8">
+                    <h2>8 - Professionalism</h2>
+                    <p>An understanding of the roles and responsibilities of the professional engineer in society, especially the primary role of protection of the public and the public interest.</p>
                     <Table8/>
                 </div>
                 <div id="table9">
+                    <h2>9 - Impact of Engineering on Society and the Environment</h2>
+                    <p>An ability to analyze social and environmental aspects of engineering activities. Such ability includes an understanding of the interactions that engineering has with the economic, social, health, safety, legal, and cultural aspects of society, the uncertainties in the prediction of such interactions; and the concepts of sustainable design and development and environmental stewardship</p>
                     <Table9/>
                 </div>
                 <div id="table10">
+                    <h2>10 - Ethics and Equity</h2>
+                    <p>An ability to apply professional ethics, accountability, and equity.</p>
                     <Table10/>
                 </div>
                 <div id="table11">
+                    <h2>11 - Economics and Project Management</h2>
+                    <p>Appropriately incorporate economics and business practices including project, risk, and change management into the practice of engineering and to understand their limitations</p>
                     <Table11/>
                 </div>
                 <div id="table12">
+                    <h2>12 - Life-Long Learning</h2>
+                    <p>An ability to identify and to address their own educational needs in a changing world in ways sufficient to maintain their competence and to allow them to contribute to the advancement of knowledge.</p>
                     <Table12/>
                 </div>
             </div>
         </div>
     )
 };
-
-
 
 function showIndicator(e){
     var table1 = document.getElementById("table1");
@@ -260,40 +287,5 @@ function showIndicator(e){
         table12.style.display = "block";
     }
 }
-
-// function showIndicator(e){
-//     console.log(e.target.id);
-//     var p = document.createElement('p');
-//     var h2 = document.createElement('h2');
-//     var description = document.getElementById("description");
-//     var p2 = document.createElement('p');
-//     var table = document.createElement('table');
-//     var tr = document.createElement('tr'); tr.id = "row1";
-//     var indicator = document.createElement('th');
-//     var unnaceptable = document.createElement('th');
-//     var belowExpect = document.createElement('th');
-//     var meetsExpect = document.createElement('th');
-//     var exceedsExpect = document.createElement('th');
-//     if(e.target.id == 1){
-//         h2.innerHTML = "1. Knowledge Base";
-//         p.innerHTML = "Demonstrated competence in university level mathematics, natural sciences, engineering fundamentals, and specialized engineering knowledge appropriate to the program.";
-//         p2.innerHTML = "(Base primarily on performance on Final Examinations for first year courses. Note this attribute is knowledge, not application, so “State Newton’s Third Law of Motion” is appropriate for this attribute whereas “Apply Newton’s Third Law of Motion to truss analysis using the Method of Sections” (or “Conduct Truss Analysis using the Method of Sections”) is Problem Analysis, Attribute 2).";
-//         indicator.innerHTML = "Indicator";
-//         unnaceptable.innerHTML = "Unnacceptable 1";
-//         belowExpect.innerHTML = "Below Expectations 2";
-//         meetsExpect.innerHTML = "Meets Expectations 3";
-//         exceedsExpect.innerHTML = "Exceeds Expectations 4";
-//         tr.appendChild(indicator);
-//         tr.appendChild(unnaceptable);
-//         tr.appendChild(belowExpect);
-//         tr.appendChild(meetsExpect);
-//         tr.appendChild(exceedsExpect);
-//         table.appendChild(tr);
-//         description.appendChild(h2);
-//         description.appendChild(p);
-//         description.appendChild(p2);
-//         description.appendChild(table);
-//     }
-// }
 
 export default Indicators;
