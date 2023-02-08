167cfc1fdc5a6969c43b057c5e456f095f8ab330
cpribaz <cpribaz@uwo.ca>
2023-02-08 11:53:06 -0500

    [ST 9] - CP: added routes to buttons

--- course/src/components/adminPanel/adminHome.js
+++ course/src/components/adminPanel/adminHome.js
@@ -5,14 +5,29 @@ import { useState } from "react";
 import { Link,Outlet, useNavigate } from "react-router-dom";
 
 function AdminHome(){
+    let navigate = useNavigate();
+
+    function goCourses(){
+        navigate('/Courses')
+    }
+    
+    function goReview(){
+        navigate('/review')
+    }
+
+    function goLogin(){
+        navigate('/')
+    }
+
     return(
         <div>
             <div class="header">
+                <button class="logoutBtn" onClick={goLogin}>Log Out</button>
                 <h3>Please Select A Type of Service:</h3>
             </div>
             <div class="buttons">
-                <button class="assignBtn">Assign Courses</button>
-                <button class="reviewBtn">Review Course Outline</button>
+                <button class="assignBtn" onClick={goCourses}>Assign Courses</button>
+                <button class="reviewBtn" onClick={goReview}>Review Course Outline</button>
             </div>
         </div>
         
