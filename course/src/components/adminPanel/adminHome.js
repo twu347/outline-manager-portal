48692277bceb1c4d5e3586fc21713433981212d0
cpribaz <cpribaz@uwo.ca>
2023-02-08 11:18:20 -0500

    [ST 9] - CP: added buttons and routed login to admin home

--- course/src/components/adminPanel/adminHome.js
+++ course/src/components/adminPanel/adminHome.js
@@ -1,15 +1,21 @@
 import React, { useEffect } from "react";
 import './adminHome.css'
 import { useState } from "react";
-import Prof from "../prof/prof";
-import CourseList from "../courseList/courseList";
+
 import { Link,Outlet, useNavigate } from "react-router-dom";
 
 function AdminHome(){
     return(
         <div>
-            <h3 class="header">Please Select A Type of Service:</h3>
+            <div class="header">
+                <h3>Please Select A Type of Service:</h3>
+            </div>
+            <div class="buttons">
+                <button class="assignBtn">Assign Courses</button>
+                <button class="reviewBtn">Review Course Outline</button>
+            </div>
         </div>
+        
     );
 }
 
