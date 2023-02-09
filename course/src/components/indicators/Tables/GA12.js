ba9dde67796142ef5776de9317c0c0b4b55694e0
cpribaz <cpribaz@uwo.ca>
2023-02-09 17:59:30 -0500

    fixed GA indicator table format

--- course/src/components/indicators/Tables/GA12.js
+++ course/src/components/indicators/Tables/GA12.js
@@ -1,35 +1,48 @@
 import React from 'react';
 
 const Table12 = () => {
-    return (
-        <table>
-        <thead>
+    const data = [
+        {
+            indicator: "LL 1: Ability to assess limitations in knowledge and skills",
+            unacceptable: "No recognition of the need to to identify and address gaps in knowledge.",
+            below: "Ineffective at objective identification of personal strengths and weaknesses.",
+            meets: "Successfully able to identify andaddress personal strengths and weaknesses.",
+            exceeds: "Meets expectations, plus: Independently displays curiosity about new technical developments and practices."
+        },
+        {
+            indicator: "LL 2: Learn independently",
+            unacceptable: "Unable to learn independently.",
+            below: "Ineffective when trying to learn independently.",
+            meets: "Able to learn independently within fields that have previously been learned at a less-challenging level.",
+            exceeds: "Meets expectations, plus: Able to learn independently within new fields that have not previously been learned."
+        },
+    ];
+
+    return(
+            <table>
+            <thead>
             <tr>
-            <th>Indicator</th>
-            <th>Unacceptable</th>
-            <th>Below Expectations</th>
-            <th>Meets Expectations</th>
-            <th>Exceeds Expectations</th>
+                <th>Indicator</th>
+                <th>Unacceptable</th>
+                <th>Below Expectations</th>
+                <th>Meets Expectations</th>
+                <th>Exceeds Expectations</th>
             </tr>
-        </thead>
-        <tbody>
-            <tr>
-            <td>LL 1 – Ability to assess limitations in knowledge and skills</td>
-            <td>No recognition of the need to to identify and address gaps in knowledge.</td>
-            <td>Ineffective at objective identification of personal strengths and weaknesses.</td>
-            <td>Successfully able to identify and address personal strengths and weaknesses.</td>
-            <td>Meets expectations, plus:<br />Independently displays curiosity about new technical developments and practices.</td>
-            </tr>
-            <tr>
-            <td>LL 2 – Learn independently</td>
-            <td>Unable to learn independently.</td>
-            <td>Ineffective when trying to learn independently.</td>
-            <td>Able to learn independently within fields that have previously been learned at a less-challenging level.</td>
-            <td>Meets expectations, plus:<br />Able to learn independently within new fields that have not previously been learned.</td>
-            </tr>
-        </tbody>
+            </thead>
+            <tbody>
+            {data.map(row => (
+                <tr key={row.indicator}>
+                <td>{row.indicator}</td>
+                <td>{row.unacceptable}</td>
+                <td>{row.below}</td>
+                <td>{row.meets}</td>
+                <td>{row.exceeds}</td>
+                </tr>
+            ))}
+            </tbody>
         </table>
-    );
+        );
 };
+
 
 export default Table12;
--- course/src/components/indicators/Tables/GA6.js
+++ course/src/components/indicators/Tables/GA6.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Table6 = () => {
-    return (
+    const data = [
+        {
+            indicator: "ITW 1- Demonstrates ability to perform responsibly",
+            unacceptable: "Student NEITHER accepts responsibilities NOR follows through on commitments, resulting in major delays to team progress.",
+            below: "Student accepts responsibilities but tasks are incomplete or incorrectly performed, requiring remediation by other team members.",
+            meets: "Student accepts responsibilities AND completes required tasks effectively and on time.",
+            exceeds: "Meets expectations, plus: Student takes initiative in supporting other team members to achieve their responsibilities."
+        },
+        {
+            indicator: "ITW 2- Demonstrates ability to contribute to team goals",
+            unacceptable: "No contribution made to establishing team goals AND/OR member responsibilities in the context of diversity of learning and working preferences.",
+            below: "Limited contribution made to establishing team goals AND/OR member responsibilities. Unable to consistently recognize diversity of learning OR working preferences",
+            meets: "Active participation in establishing team goals AND member responsibilities that recognizes the diversity of learning and working preferences.",
+            exceeds: "Meets expectations, plus: Student takes initiative in identifying areas for improvement of team dynamics by, e.g. generating ideas, providing constructive feedback to team members, etc."
+        },
+        {
+            indicator: "ITW3-Demonstrate ability to evaluate peer and self performance based on team effectiveness",
+            unacceptable: "Does not participate in peer evaluation or provides inadequate judgement of self/peer performance",
+            below: "Participates in peer/self evaluation, providing inconsistent judgement of self/peer performance",
+            meets: "Participates in peer/self evaluation, providing accurate judgement of self/peer performance",
+            exceeds: "Meets expectations, plus: Identifies methods to improve team effectiveness based on observed weaknesses."
+        }
+    ];
+    return(
         <table>
         <thead>
             <tr>
@@ -13,70 +36,15 @@ const Table6 = () => {
             </tr>
         </thead>
         <tbody>
-            <tr>
-            <td>ITW 1- Demonstrates ability to perform responsibly</td>
-            <td>
-                Student NEITHER accepts responsibilities NOR follows through on
-                commitments, resulting in major delays to team progress.
-            </td>
-            <td>
-                Student accepts responsibilities but tasks are incomplete or
-                incorrectly performed, requiring remediation by other team members.
-            </td>
-            <td>
-                Student accepts responsibilities AND completes required tasks
-                effectively and on time.
-            </td>
-            <td>
-                Meets expectations, plus: Student takes initiative in supporting
-                other team members to achieve their responsibilities.
-            </td>
+            {data.map((row, index) => (
+            <tr key={index}>
+                <td>{row.indicator}</td>
+                <td>{row.unacceptable}</td>
+                <td>{row.below}</td>
+                <td>{row.meets}</td>
+                <td>{row.exceeds}</td>
             </tr>
-            <tr>
-            <td>ITW 2- Demonstrates ability to contribute to team goals</td>
-            <td>
-                No contribution made to establishing team goals AND/OR member
-                responsibilities in the context of diversity of learning and working
-                preferences.
-            </td>
-            <td>
-                Limited contribution made to establishing team goals AND/OR member
-                responsibilities. Unable to consistently recognize diversity of
-                learning OR working preferences
-            </td>
-            <td>
-                Active participation in establishing team goals AND member
-                responsibilities that recognizes the diversity of learning and working
-                preferences.
-            </td>
-            <td>
-                Meets expectations, plus: Student takes initiative in identifying
-                areas for improvement of team dynamics by, e.g. generating ideas,
-                providing constructive feedback to team members, etc.
-            </td>
-            </tr>
-            <tr>
-            <td>
-                ITW3-Demonstrate ability to evaluate peer and self performance based
-                on team effectiveness
-            </td>
-            <td>
-                Does not participate in peer evaluation or provides inadequate
-                judgement of self/peer performance
-            </td>
-            <td>
-                Participates in peer/self evaluation, providing inconsistent
-                judgement of self/peer performance
-            </td>
-            <td>
-                Participates in peer/self evaluation, providing accurate judgement
-                of self/peer performance
-            </td>
-            <td>
-                Meets expectations, plus: Identifies methods to improve team
-                effectiveness based on observed weaknesses.
-            </td>
-            </tr>
+            ))}
         </tbody>
         </table>
     );
