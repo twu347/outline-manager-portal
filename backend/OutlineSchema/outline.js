82a8ef5aa928b99e9849f93f5b86213576e0e848
cpribaz <cpribaz@uwo.ca>
2023-03-09 19:17:07 -0500

    [ST 7] - CP: changed the comment attribute to array of strings

--- backend/OutlineSchema/outline.js
+++ backend/OutlineSchema/outline.js
@@ -21,10 +21,10 @@ const Outline = new mongoose.Schema({
     yearFrom:{
         type:Number, 
     }, 
-    comment:{
+    comment:[{
         type: String,
         required: false,
-    },
+    }],
     yearTo:{
         type:Number, 
     },
